78e02bfd63ef27f17aa2372c8252ffcab365eb3c
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2019-10-23 13:15:19 -0700

    first commit

---
+++ weather.js
@@ -0,0 +1,9 @@
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9cb57c03913866531529917ca50d9aef", function(data){
+    console.log(data);
+});
+
+
+
+
+
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
